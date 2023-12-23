8a0ad766e40ccdb1c079c33aad78665cbcd18c2c
liamm <gdschanu.femember@gmail.com>
2023-12-23 18:09:38 +0700

    sign up validate

--- scripts/signup.js
+++ scripts/signup.js
@@ -10,21 +10,32 @@ const rePassError = document.getElementById("rePassError");
 const submitBtn = document.getElementById("submitBtn");
 
 // validate form
-submitBtn.addEventListener("click", () => {
-  console.log(emailInput.value);
+submitBtn.addEventListener("click", (event) => {
+  // prevent default submit event
+  event.preventDefault();
+
+  // check valid email input
   if (!emailInput.value) {
     emailErr.classList.remove("d-none");
+    return;
   }
 
   // password schema
   // at least one uppercase letter and is at least 8 characters long
   const schema = new RegExp("^(?=.*[A-Z]).{8,}$");
 
+  // check valid password
   if (!schema.test(passwordInput.value)) {
     passwordErr.classList.remove("d-none");
+    return;
   }
 
-  if (rePassInput !== passwordInput) {
+  // check valid re-enter password
+  if (rePassInput.value !== passwordInput.value) {
     rePassError.classList.remove("d-none");
+    return;
   }
+
+  // when everything is oke -> redirect to homepage
+  window.location.replace("/");
 });
